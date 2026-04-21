a78a8f38c13c9516ff172fdfa25f45d3a2c4a6ad
sggi-76 <122995865+sggi-76@users.noreply.github.com>
2026-04-21 16:43:11 +0100

    Update Sensitivity_beta_x_Linear_correct_ES_second_figure.R
    Plots for linear sensitivity plot

--- Sensitivity_beta_x_Linear_correct_ES_second_figure.R
+++ Sensitivity_beta_x_Linear_correct_ES_second_figure.R
@@ -1,10 +1,7 @@
   library(ggplot2)
   library(tidyverse)
-  #library(metR)
   library(plotly)
-  setwd("~/Dropbox (LSE Statistics)/Elena/R code")
-  #setwd("C:/Users/ElenaStanghellini/Dropbox/Elena/R code")
-  
+
   #Code for beta_x contour
   # x axis is p(U=0|Y=0,X=0) = p_ux0
   # y axis is p(U=0|Y=0, X=1)
